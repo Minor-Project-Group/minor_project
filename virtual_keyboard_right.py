da30c0b8d3da05ed0b1e5daa1482b0042a10051b
Shubham-1105 <42351806+Shubham-1105@users.noreply.github.com>
2020-02-09 01:38:04 +0530

    Add files via upload

---
+++ virtual_keyboard_right.py
@@ -0,0 +1,90 @@
+import cv2
+import numpy as np
+
+keyboard = np.zeros((600, 1000, 3), np.uint8)
+
+keys_set_1 = {0: "Y", 1: "U", 2: "I", 3: "O", 4: "P",
+              5: "H", 6: "J", 7: "K", 8: "L", 9: "<",
+              10: "N", 11: "M", 12: ">", 13: ",", 14: "."}
+
+def letter(letter_index, text, letter_light):
+    
+    if letter_index == 0:
+        x = 0
+        y = 0
+    elif letter_index == 1:
+        x = 200
+        y = 0
+    elif letter_index == 2:
+        x = 400
+        y = 0
+    elif letter_index == 3:
+        x = 600
+        y = 0
+    elif letter_index == 4:
+        x = 800
+        y = 0
+    elif letter_index == 5:
+        x = 0
+        y = 200
+    elif letter_index == 6:
+        x = 200
+        y = 200
+    elif letter_index == 7:
+        x = 400
+        y = 200
+    elif letter_index == 8:
+        x = 600
+        y = 200
+    elif letter_index == 9:
+        x = 800
+        y = 200
+    elif letter_index == 10:
+        x = 0
+        y = 400
+    elif letter_index == 11:
+        x = 200
+        y = 400
+    elif letter_index == 12:
+        x = 400
+        y = 400
+    elif letter_index == 13:
+        x = 600
+        y = 400
+    elif letter_index == 14:
+        x = 800
+        y = 400
+
+    width = 200
+    height = 200
+    th = 3 
+    if letter_light is True:
+        cv2.rectangle(keyboard, (x + th, y + th), (x + width - th, y + height - th), (255, 255, 255), -1)
+    else:
+        cv2.rectangle(keyboard, (x + th, y + th), (x + width - th, y + height - th), (255, 0, 0), th)
+
+    
+    font_letter = cv2.FONT_HERSHEY_PLAIN
+    font_scale = 10
+    font_th = 4
+    text_size = cv2.getTextSize(text, font_letter, font_scale, font_th)[0]
+    width_text, height_text = text_size[0], text_size[1]
+    text_x = int((width - width_text) / 2) + x
+    text_y = int((height + height_text) / 2) + y
+    cv2.putText(keyboard, text, (text_x, text_y), font_letter, font_scale, (255, 0, 0), font_th)
+
+
+
+for i in range(15):
+    if i == 7:
+        light = True
+    else:
+        light = False
+    letter(i, keys_set_1[i], light)
+
+
+
+cv2.imshow("keyboard", keyboard)
+cv2.imwrite("keyboard_left.jpg", keyboard)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
